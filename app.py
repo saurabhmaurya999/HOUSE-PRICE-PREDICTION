c3d6bee3931de3b3505ec1e284503b37a9d73026
Saurabh Maurya <saurabhmaurya46577@gmail.com>
2025-09-13 21:27:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+import pickle as pk
+import numpy as np
+
+
+model = pk.load(open(r"C:\Users\Saurabh Maurya\Documents\PROJECT OF ML\House_prediction_model.pkl", "rb"))
+
+st.header("House Price Predictor")
+
+data = pd.read_csv(r"C:\Users\Saurabh Maurya\Documents\PROJECT OF ML\Cleaned_data.csv")
+
+
+Area = st.number_input("Area(sqft):",min_value=500)
+Bedrooms = st.number_input("Number of bedrooms:",min_value=1)
+Bathrooms = st.number_input("Number of bathrooms:",min_value=1)
+Mainroad = st.selectbox("Near Main Road :?",["yes","no"])
+Guestroom = st.selectbox("GuestRoom vailable?",["yes","no"])
+
+input =  pd.DataFrame([[Area, Bedrooms, Bathrooms, Mainroad, Guestroom]],columns=['area', 'bedrooms', 'bathrooms', 'mainroad', 'guestroom'])
+if st.button("Predict Price"):
+    prediction = model.predict(input)
+    price = prediction[0] # ye list ya array se simple data banata hai like  [ 23434] - 23434
+    st.success(f"House Price:₹{int(price)}") 
+
+
+
+
